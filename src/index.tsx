9b5bfe12a5459dc65685c155e15e96bffc1a5fd4
Jakub Lobos <saltver2@onet.eu>
2022-12-29 13:16:05 +0100

    FIX: gh pages

--- src/index.tsx
+++ src/index.tsx
@@ -8,9 +8,9 @@ const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 
 root.render(
   <React.StrictMode>
-    <HashRouter>
+    <BrowserRouter>
       <App />
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
