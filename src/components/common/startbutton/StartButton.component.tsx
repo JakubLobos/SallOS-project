84dabff4b8c20626cca4c3052bd98b65bc3af4cb
Jakub Lobos <saltver2@onet.eu>
2022-12-30 15:44:05 +0100

    FIX: StartButton href style change

--- src/components/common/startbutton/StartButton.component.tsx
+++ src/components/common/startbutton/StartButton.component.tsx
@@ -2,13 +2,15 @@ import { getLoadingPageLink } from "../../service/Links.service"
 import StyledStartButton from "./StartButton.style"
 import buttonImg from "../../../img/static/StartButton.png"
 import clickSound from "../../../sounds/common/click-124467.mp3"
+import { useNavigate } from "react-router-dom"
 
 
 const StartButton = () => {
     const startButtonAudio = new Audio(clickSound);
+    const navigate = useNavigate()
     return (
        
-            <StyledStartButton onMouseOver={() => startButtonAudio.play()} href={getLoadingPageLink()}>
+            <StyledStartButton onClick={() => navigate(getLoadingPageLink())} onMouseOver={() => startButtonAudio.play()}>
                 <img
                     alt="start button img"
                     src={buttonImg}
